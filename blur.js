99661224489d9646c250106d2c1f57471c99f38f
Danny Chahla <dchahla@gmail.com>
2014-07-16 22:26:21 -0500

    timed after 30 seconds to unblur

--- blur.js
+++ blur.js
@@ -2,12 +2,13 @@
 
 $(document).ready(function () {
 	//window.alert("sometext");
-
-  $('.homeBtn').on('click', function() {
+setTimeout(function() { 
+//alert("Hello")}, 3000);
+  //$('.homeBtn').on('click', function() {
   	//$('.Blur').removeClass('blur');
   	//$('.nonBlur').addClass('blur');
     $('.blur').toggleClass('nonBlur');
     //$('.login').slideToggle(500);
-});
+}, 3000);
 
 });
