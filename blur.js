e3d0336484a0bb0248e77f91db5186a9b67a163a
Danny Chahla <dchahla@gmail.com>
2014-07-16 22:10:13 -0500

    click to toogle blur

---
+++ blur.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function () {
+	//window.alert("sometext");
+
+  $('.homeBtn').on('click', function() {
+  	//$('.Blur').removeClass('blur');
+  	//$('.nonBlur').addClass('blur');
+    $('.blur').toggleClass('nonBlur');
+    //$('.login').slideToggle(500);
+});
+
+});
